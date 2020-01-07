c808828b9fb0bab87ea8c3f771589e53a7d3f798
licong <licongcong@ybm100.com>
2020-01-07 10:34:11 +0800

    增加mybatis自动生成目录

---
+++ gencoder/run-mysql.bat
@@ -0,0 +1,7 @@
+@echo off
+java -jar  D:\mavenlib\org\mybatis\generator\mybatis-generator-core\1.3.7\mybatis-generator-core-1.3.7.jar  -configfile E:\download\gencoder\generatorConfig.xml -overwrite
+pause
+
+
+
+
---
+++ gencoder/run.bat
@@ -0,0 +1,7 @@
+@echo off
+java -jar  F:\gencoder\mybatis-generator-core-1.3.2.jar  -configfile F:\gencoder\generatorConfig.xml -overwrite
+pause
+
+
+
+
